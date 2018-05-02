dd058cf22b5798f2a48bad0cf229501b73681373
Atlantis <djoron@yahoo.ca>
2018-05-01 22:16:24 -0400

    Corrected types for Chart model

--- src/main/java/com/evdosoft/stocktechsys/models/Chart.java
+++ src/main/java/com/evdosoft/stocktechsys/models/Chart.java
@@ -5,6 +5,9 @@
  */
 package com.evdosoft.stocktechsys.models;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  * Model to download price History from IEX
  * Example of call: https://api.iextrading.com/1.0/stock/aapl/chart/5y
@@ -26,114 +29,62 @@ package com.evdosoft.stocktechsys.models;
  */
 public class Chart {
  
-    protected String date;
-    protected String open;
-    protected String high;
-    protected String low;
-    protected String close;
-    protected String volume;
-    protected String unadjustedVolume;
-    protected String change;
-    protected String changePercent;
-    protected String vwap;
+    protected Date date;
+    protected BigDecimal open;
+    protected BigDecimal high;
+    protected BigDecimal low;
+    protected BigDecimal close;
+    protected Integer volume;
+    protected Integer unadjustedVolume;
+    protected float change;
+    protected float changePercent;
+    protected BigDecimal vwap;
     protected String label;
-    protected String changeOverTime;
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
+    protected float changeOverTime;
+    
+    public void setDate(Date date) {
         this.date = date;
     }
 
-    public String getOpen() {
-        return open;
-    }
-
-    public void setOpen(String open) {
+    public void setOpen(BigDecimal open) {
         this.open = open;
     }
 
-    public String getHigh() {
-        return high;
-    }
-
-    public void setHigh(String high) {
+    public void setHigh(BigDecimal high) {
         this.high = high;
     }
 
-    public String getLow() {
-        return low;
-    }
-
-    public void setLow(String low) {
+    public void setLow(BigDecimal low) {
         this.low = low;
     }
 
-    public String getClose() {
-        return close;
-    }
-
-    public void setClose(String close) {
+    public void setClose(BigDecimal close) {
         this.close = close;
     }
 
-    public String getVolume() {
-        return volume;
-    }
-
-    public void setVolume(String volume) {
+    public void setVolume(Integer volume) {
         this.volume = volume;
     }
 
-    public String getUnadjustedVolume() {
-        return unadjustedVolume;
-    }
-
-    public void setUnadjustedVolume(String unadjustedVolume) {
+    public void setUnadjustedVolume(Integer unadjustedVolume) {
         this.unadjustedVolume = unadjustedVolume;
     }
 
-    public String getChange() {
-        return change;
-    }
-
-    public void setChange(String change) {
+    public void setChange(float change) {
         this.change = change;
     }
 
-    public String getChangePercent() {
-        return changePercent;
-    }
-
-    public void setChangePercent(String changePercent) {
+    public void setChangePercent(float changePercent) {
         this.changePercent = changePercent;
     }
 
-    public String getVwap() {
-        return vwap;
-    }
-
-    public void setVwap(String vwap) {
+    public void setVwap(BigDecimal vwap) {
         this.vwap = vwap;
     }
 
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public String getChangeOverTime() {
-        return changeOverTime;
-    }
-
-    public void setChangeOverTime(String changeOverTime) {
+    public void setChangeOverTime(float changeOverTime) {
         this.changeOverTime = changeOverTime;
     }
-   
-   
 }
+
+    
