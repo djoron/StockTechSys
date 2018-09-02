57287b138ca35c0190f2370417b494e472aa4658
Atlantis <djoron@yahoo.ca>
2018-09-02 09:58:39 -0400

    Async - Save symbol list. Wasn't done before.

--- src/main/java/com/evdosoft/stocktechsys/AsyncBootstrapping.java
+++ src/main/java/com/evdosoft/stocktechsys/AsyncBootstrapping.java
@@ -1,17 +1,44 @@
 package com.evdosoft.stocktechsys;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.evdosoft.stocktechsys.StockTechSysConstants.TypeListDownload;
+import com.evdosoft.stocktechsys.models.Symbol;
+import com.evdosoft.stocktechsys.service.PriceHistoryService;
+import com.evdosoft.stocktechsys.service.SqlDatabaseService;
+import com.evdosoft.stocktechsys.service.SymbolService;
 import com.evdosoft.stocktechsys.service.async.CompanyServiceAsync;
 
 @Component
 public class AsyncBootstrapping {
 
     @Autowired
+    private SqlDatabaseService sqlDatabaseService;
+	
+    @Autowired
+    private SymbolService symbolService;
+	
+    @Autowired
+    private PriceHistoryService priceHistoryService;
+	
+    @Autowired
     private CompanyServiceAsync companyServiceAsync;
 
-    public void fetchData() {
+    public void fetchData() throws Exception {
+	
+	// Initialize Symbol list and database. Symbol list will be used later to generate 
+	// company list
+        List<Symbol> symbolList = new ArrayList<>(); 
+        sqlDatabaseService.createSqlDb();
+        
+        symbolList = symbolService.getSymbolList();
+        symbolService.saveSymbolList(TypeListDownload.ORIGINAL, symbolList);
+	
 	companyServiceAsync.fetchAndSaveCompanyList();
+	
     }
 }
